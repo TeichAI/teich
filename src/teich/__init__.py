0f2220f489a3a387dcb0684b3bb388e79b62d8e4
Arman Rafiee <aran.rafiee@gmail.com>
2026-06-06 04:34:58 -0400

    chore: bump version to 0.1.1a80 in project metadata and source files

--- src/teich/__init__.py
+++ src/teich/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__version__ = "0.1.1a79"
+__version__ = "0.1.1a80"
 
 from .audit import SFTAuditReport, audit_sft_dataset
 from .config import Config, load_config
